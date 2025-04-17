1c9b23b3b23706d00a223c362a5875c465f3d8c3
Jack Liang <86848773+jack-200@users.noreply.github.com>
2025-04-17 16:33:32 -0700

    add css styling to NAVBAR

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+
 export const Navbar = () => {
     return (
-        <div> NAVBAR
-            <Link to="/"> Home </Link>
-            <Link to="/Login"> Login </Link>
-            <Link to="/Log"> Log </Link>
-            <Link to="/Calendar"> Calendar </Link>
-            <Link to="/Weather"> Weather </Link>
-        </div>
+        <nav className="navbar">
+            <ul className="navbar-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/Login">Login</Link></li>
+                <li><Link to="/Log">Log</Link></li>
+                <li><Link to="/Calendar">Calendar</Link></li>
+                <li><Link to="/Weather">Weather</Link></li>
+                <li><Link to="/Statistics">Statistics</Link></li>
+            </ul>
+        </nav>
     );
-
-
-}
+};
