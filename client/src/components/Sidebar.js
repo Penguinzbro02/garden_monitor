228602de78fc2da23598fbe71aa4070fc384d831
yk60 <youkyoung546@gmail.com>
2025-04-18 15:47:45 -0700

    Update sidebar css

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,14 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Sidebar.css';
-import { SidebarData } from './SidebarData'
+import { SidebarData } from './SidebarData';
 
 export const Sidebar = () => {
+    const currRoute = useLocation();
+
     return (
         <div className="Sidebar">
             <ul className="SidebarList">
+                {/* For each sidebar object, check if the current route and sidebar link matches */}
                 {SidebarData.map((val, key) => {
+                    const isActive = currRoute.pathname === val.link;
                     return (
-                        <li key={key} className="row">
+                        // add the active class to change color of the selected menu item
+                        <li key={key} className={`row ${isActive ? 'active' : ''}`}>
                             <Link to={val.link} className="row">
                                 {val.title}
                             </Link>
